2fd93ffd980598e25908a936a3cdf4e2cea98f81
wordpressperson <52249958+wordpressperson@users.noreply.github.com>
2022-09-30 21:27:55 +0100

    Update Navbar.js
    added space

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className="logo">
         <SiAnaconda color="#fff" size={33} />
         <p className="logo-text">
-          <span>GCK</span>
+          <span>&nbsp; GCK</span>
         </p>
       </div>
       <menu>
